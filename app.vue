2de535a24b6dea878af419f68b9e441020dab714
gp hong <hgoeshard@gmail.com>
2025-05-09 20:52:24 +0900

    feat: add setting

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <Todo />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Todo from '~/components/Todo.vue';
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Arial', 'Helvetica', sans-serif;
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+</style>
